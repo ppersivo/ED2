8266a0ff2873d0cc7f414285779ba1c31baffbe4
Paulo Persivo <paulo.persivo@gmail.com>
2012-08-31 15:06:47 -0300

    Estrutura de dados em C++ / Lista encadeada
    Continuando a implementação da lista encadeada em C++.

--- C++/ListaEncadeada.cpp
+++ C++/ListaEncadeada.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 
-void add(const_T &x, bool no_fim=true);
+//void addNodo(nodo &p, int valor);
+
+class nodo{
+    public:
+    nodo(){
+        next=0;
+    }
+    nodo(int i, nodo *in = 0){
+        info = i;
+        next = in;
+    }
+    int info;
+    nodo *next;
+};
 
 int main(){
+    //criar lista
+    nodo *lista = new nodo(0);
+    //criar nó
+    y=200;
 
-}
-
-void add(const_T &x, bool no_fim=true){
-    nodo *q = new nodo(x);
-    if(cnt==0){
-        ptraiz=q;
-    }else{
-        if(no_fim){
-            nodo *p=ptraiz;
-            while(p->prox!=0) p=p->prox;
-            p->prox=q;
+    while(y>=0){
+        for(int i=0; i<10000; i++){
+            addNodo(&lista,0);
         }
+        y--;
     }
-    cnt++;
+    //posicionar nó na cabeça da lista
+    return 0;
 }
+/*
+void addNodo(nodo &p, int valor){
+
+}
+*/
