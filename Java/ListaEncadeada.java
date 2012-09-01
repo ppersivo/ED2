8034624abcc0cd1f514d834e1471d334a9ef5e3d
Paulo Persivo <paulo.persivo@gmail.com>
2012-09-01 11:24:22 -0300

    Estruturas de dados em Java / Lista encadeada
    Legibilidade do código melhorada

--- Java/ListaEncadeada.java
+++ Java/ListaEncadeada.java
@@ -5,20 +5,26 @@
  * @author 
  * @version 1.00 2012/8/31
  */
+
 import java.util.LinkedList;
 
 public class ListaEncadeada {
 
     public static void main(String[] args) {
     	LinkedList<Integer> lista = new LinkedList<Integer>();
-    	int y = 200;
-    	while(y>=0){
-    		for(int i = 0; i<lista.size(); i++){
-    			lista.add(0);
-    		}
-    		y--;
+    	
+        int y = 200;
+    	
+        while(y>=0){
+    		
+            for(int i = 0; i<lista.size(); i++){
+    		lista.add(0);
+            }
+            
+            y--;
+            
     	}
+        
     }
-    
-    
+     
 }
